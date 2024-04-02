0468a7b5d0214bb189a18e3138e5b0ea8e5249f7
ystass <stepanischevyy@mail.ru>
2024-04-02 13:59:11 +0400

    add main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# def user_contact():
+from src.class_api import FromHHru
+from src.methods import ListVacancies
+
+user_vacancy = input('Введите вакансию для поиска на сайте hh.ru: \n')
+hh = FromHHru()
+vacancies = hh.get_vacancies(user_vacancy)
+fv = ListVacancies()
+fv1 = fv.save_vacancies(vacancies)
+
+fv2 = fv.delete_vacancy()
+print(fv2)
