b77c846c018f84a4b865a654171d2b5150fef133
ystass <stepanischevyy@mail.ru>
2024-04-02 22:31:43 +0400

    добавил обращение с пользователем

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from src.class_api import FromHHru
 from src.methods import ListVacancies
+import json
 
 user_vacancy = input('Введите вакансию для поиска на сайте hh.ru: \n')
 hh = FromHHru()
 vacancies = hh.get_vacancies(user_vacancy)
+
 fv = ListVacancies()
 fv1 = fv.save_vacancies(vacancies)
 
-fv2 = fv.delete_vacancy()
-print(fv2)
+# name_vac = input('Введите название вакансии: \n')
+# fv3 = fv.add_vacancy(name_vac)
+#
+# name_exit = input('Завершим и очистим файл вакансий да/нет : \n')
+# if name_exit == 'да':
+#     fv4 = fv.delete_vacancy()
+# print('Выбранные вакансии в my_vacancies.json')
+# name_criterion = input('Введите критерий для отбора вакансий: \n')
+# fv4 = fv.get_data(name_criterion)
