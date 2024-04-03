1fa65594eddd12fee05a4d90c65b94ab5e6c87e6
ystass <stepanischevyy@mail.ru>
2024-04-03 20:50:28 +0400

    add sorted

--- src/sorting.py
+++ src/sorting.py
@@ -1,3 +1,12 @@
 from src.translation import vacancies
 
-def sorting():
+
+def sorting(n):
+    sorted_list = sorted(vacancies, key=lambda x: x['Зарплата'], reverse=True)
+    sorted_vac = sorted_list[:n]
+    return sorted_vac
+
+
+s_v = sorting(5)
+
+print(s_v)
