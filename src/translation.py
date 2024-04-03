7ee5369b1a8bb4880f43edf408831f29c4ca97f6
ystass <stepanischevyy@mail.ru>
2024-04-03 21:24:40 +0400

    изменение условий

--- src/translation.py
+++ src/translation.py
@@ -18,12 +18,15 @@ def vac_user():
                 salary = vac["salary"]["to"]
             elif vac["salary"]["from"] is not None and vac["salary"]["to"] is None:
                 salary = vac["salary"]["from"]
-                currency = vac["salary"]["currency"]
-            url = vac["alternate_url"]
-            if vac["snippet"]["requirement"]:
-                requirement = vac["snippet"]["requirement"]
-            else:
-                requirement = "Информация отсутствует"
+        if vac["salary"]["currency"]:
+            currency = vac["salary"]["currency"]
+        else:
+            currency = "Валюта не определена"
+        url = vac["alternate_url"]
+        if vac["snippet"]["requirement"]:
+            requirement = vac["snippet"]["requirement"]
+        else:
+            requirement = "Информация отсутствует"
         #lv = FromVacancy
         user_vac.append(FromVacancy(name, salary, currency, url, requirement))
     print(user_vac)
