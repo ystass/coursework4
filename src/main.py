8ddee5636116f5f0c5ee361e2776377eb544a2fb
ystass <stepanischevyy@mail.ru>
2024-04-04 17:42:37 +0400

    доработка

--- src/main.py
+++ src/main.py
@@ -3,20 +3,26 @@ from src.class_vacancy import FromVacancy
 from src.methods import ListVacancies
 import json
 
+from src.sorting import sorting
+
 user_vacancy = input('Введите вакансию для поиска на сайте hh.ru: \n')
 hh = FromHHru()
 vacancies = hh.get_vacancies(user_vacancy)
 
-
 fv = ListVacancies()
 fv1 = fv.save_vacancies(vacancies)
-#
-# name_vac = input('Введите название вакансии: \n')
-# fv3 = fv.add_vacancy(name_vac)
-# #
+
+name_vac = input('Введите название вакансии: \n')
+fv3 = fv.add_vacancy(name_vac)
+
+name_criterion = input('Введите критерий для отбора вакансий: \n')
+fv4 = fv.get_data(name_criterion)
+
+n = input('Введите количество вакансий для просмотра: \n')
+sorting(n)
+
 # name_exit = input('Завершим и очистим файл вакансий да/нет : \n')
 # if name_exit == 'да':
 #     fv4 = fv.delete_vacancy()
 # print('Выбранные вакансии в my_vacancies.json')
-name_criterion = input('Введите критерий для отбора вакансий: \n')
-fv4 = fv.get_data(name_criterion)
+
