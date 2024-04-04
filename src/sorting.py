e2add6a8f3bc095cbf8c24776c4f74b23e47c3dc
ystass <stepanischevyy@mail.ru>
2024-04-04 17:42:50 +0400

    доработка

--- src/sorting.py
+++ src/sorting.py
@@ -1,12 +1,14 @@
-from src.translation import vacancies
+from src.translation import vac_user
 
+user_vac = vac_user()
 
 def sorting(n):
-    sorted_list = sorted(vacancies, key=lambda x: x['Зарплата'], reverse=True)
+    sorted_list = sorted(user_vac, key=lambda x: x['Зарплата'], reverse=True)
     sorted_vac = sorted_list[:n]
+    print(sorted_vac)
     return sorted_vac
 
 
-s_v = sorting(5)
-
-print(s_v)
+# s_v = sorting(5)
+#
+# print(s_v)
